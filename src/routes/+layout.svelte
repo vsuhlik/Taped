ae508bdc9abc729db8b8db00aa4cd688bb733e10
Artax <mattisbogus@gmail.com>
2026-09-17 20:32:50 -0400

    Fix apple-touch-icon location

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,11 +8,11 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<link rel="manifest" href="/manifest.json" />
-	<link rel="apple-touch-icon" href="/icons/icon.svg" />
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 	<meta name="application-name" content="Taped" />
 	<meta name="apple-mobile-web-app-capable" content="yes" />
-	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-	<meta name="apple-mobile-web-app-title" content="Taped" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+	<meta name="apple-mobile-web-app-title" content="Je t'aime" />
 	<meta name="theme-color" content="#111827" />
 </svelte:head>
 {@render children()}
